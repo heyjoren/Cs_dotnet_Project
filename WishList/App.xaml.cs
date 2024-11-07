66a86f004d274b582f881dcb2ea322290449d763
heyjoren <heyvaertjoren@gmail.com>
2024-11-07 15:39:14 +0100

    maui code werkt

---
+++ WishList/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WishList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
