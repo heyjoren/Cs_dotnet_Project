772b644c0f53139764eb6af03e12e693a69d769e
heyjoren <heyvaertjoren@gmail.com>
2024-11-13 13:18:52 +0100

    Model voor item op te slaan gemaakt. en local moc data

---
+++ WishList/Local moc data/MockDataStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WishList.Model;
+
+namespace WishList.Local_moc_data
+{
+    public static class MockDataStore 
+    {
+        public static List<Item> ItemsList { get; } = new List<Item>();
+
+        public static ObservableCollection<Item> ObservableItemsList { set; get; } = new ObservableCollection<Item>();
+        //public static List<string> ItemsList { get; } = new List<string>();
+    }    
+}
---
+++ WishList/Model/Item.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishList.Model
+{
+    public class Item
+    {
+        private static int currentId = 0;
+        public int Id { get; private set; }
+        public string Naam { set; get; }
+        public float Bedrag { set; get; }
+        public string Beschrijving { set; get; }
+        public string Bedrijf { set; get; }
+        public DateTime DatumToegevoegd { set; get; }
+
+        public Item()
+        {
+            currentId++;
+            Id = currentId;
+            Naam = "";
+            Bedrag = 0;
+            Beschrijving = "";
+            Bedrijf = "";
+            DatumToegevoegd = DateTime.Now;
+        }
+
+        public Item(string _Naam, float _Bedrag, string _Beschrijving, string _Bedrijf)
+        {
+            currentId++;
+            Id = currentId;
+            Naam = _Naam;
+            Bedrag = _Bedrag;
+            Beschrijving = _Beschrijving;
+            Bedrijf = _Bedrijf;
+            DatumToegevoegd = DateTime.Now;
+        }
+
+        public Item(string _Naam, float _Bedrag, string _Beschrijving, string _Bedrijf, DateTime _DatumToegevoegd)
+        {
+            currentId++;
+            Id = currentId;
+            Naam = _Naam;
+            Bedrag = _Bedrag;
+            Beschrijving = _Beschrijving;
+            Bedrijf = _Bedrijf;
+            DatumToegevoegd = _DatumToegevoegd;
+        }
+
+        public Item(int _Id, string _Naam, float _Bedrag, string _Beschrijving, string _Bedrijf, DateTime _DatumToegevoegd)
+        {
+            Id = _Id;
+            currentId = _Id;
+            Naam = _Naam;
+            Bedrag = _Bedrag;
+            Beschrijving = _Beschrijving;
+            Bedrijf = _Bedrijf;
+            DatumToegevoegd = _DatumToegevoegd;
+        }
+
+        public override String ToString()
+        {
+            // return JsonSerializer.Serialize(this);
+            return $"Id: {Id}, Naam: {Naam}, Bedrag: €{Bedrag}, Bedrijf: {Bedrijf}, Beschrijving: {Beschrijving}, Datum: {DatumToegevoegd:dd/MM/yyyy}";
+        }
+    }
+}
