7e2317b699d28bf64e4af70b88106dba13038268
heyjoren <heyvaertjoren@gmail.com>
2024-11-26 10:45:12 +0100

    Service folder gemaakt voor in file mock data en api mock data
    in project mock data verplaatst naar deze folder

---
+++ WishList/Services/ApiMockData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WishList.Services
+{
+    internal class ApiMockData
+    {
+    }
+}
